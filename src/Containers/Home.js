84b650b89bc99e117179d0aefbf8042402923bc9
allenorton <norton.allene@gmail.com>
2020-10-13 10:29:36 -0500

    added my saved wines header to home page

--- src/Containers/Home.js
+++ src/Containers/Home.js
@@ -3,10 +3,15 @@ import Logout from '../Components/Logout'
 
 class Home extends Component {
   render() {
+    let user = JSON.parse(localStorage.getItem('user'))
     return (
       <div className="home">
         <h2>Viness Home Page</h2>
-        {/* <h3>{JSON.parse(localStorage.getItem('user')).name}</h3> */}
+        <h3>{user.name}</h3>
+        <img src={user.imageUrl} alt={user.name} />
+        <div className='saved-wines'>
+          <h3>My saved wines:</h3>
+        </div>
         <Logout />
       </div>
     );
