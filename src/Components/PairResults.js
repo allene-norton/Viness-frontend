96d0299dcd36b0fe4c0c588599a937364e67199b
allenorton <norton.allene@gmail.com>
2020-10-12 19:22:54 -0500

    error handling for wine pairing

--- src/Components/PairResults.js
+++ src/Components/PairResults.js
@@ -13,32 +13,33 @@ class PairResults extends Component {
     desc = this.pairObj.pairingText
     bottles = this.pairObj.productMatches
     renderPairs = (pairs) => {
-        if (Object.keys(this.pairObj).length >= 1)
-        {return pairs.map(wine => <li>{wine}</li>)}
-        else {return undefined}
+        if (Object.keys(this.pairObj).includes('status')) { return undefined }
+        else if (Object.keys(this.pairObj).includes('pairedWines')) { return pairs.map(wine => <li>{wine}</li>) }
+        else { return undefined }
     }
     renderWines = (wines) => {
-        if (Object.keys(this.pairObj).length >= 1)
-        {return wines.map(wine => <RecResults key={wine.id} wine={wine} postWine={this.props.postWine} />)}
-        else {return undefined}
+        if (Object.keys(this.pairObj).includes('status')) { return <p>{this.pairObj.message}</p> }
+        else if (Object.keys(this.pairObj).includes('pairedWines')) {
+            if (wines.length === 0) { return <p>Could not find a match. Please try again with a dish, ingredient, or cuisine type.</p> } else { return wines.map(wine => <RecResults key={wine.id} wine={wine} postWine={this.props.postWine} />) }}
+        else { return undefined }
+        }
+
+        render() {
+            console.log(this.pairObj)
+            return (
+                <div className="rec-results">
+                    <div className="paired-wines">
+                        <ul>
+                            {this.renderPairs(this.pairedWines)}
+                        </ul>
+                    </div>
+                    <p>{this.desc}</p>
+                    <div className="paired-bottle">
+                        {this.renderWines(this.bottles)}
+                    </div>
+                </div>
+            );
+        }
     }
 
-    render() {
-        console.log(this.pairObj)
-        return (
-            <div className="rec-results">
-                <div className="paired-wines">
-                    <ul>
-                        {this.renderPairs(this.pairedWines)}
-                    </ul>
-                </div>
-                <p>{this.desc}</p>
-                <div className="paired-bottle">
-                    {this.renderWines(this.bottles)}
-                </div>
-            </div>
-        );
-    }
-}
-
-export default PairResults;
+    export default PairResults;
