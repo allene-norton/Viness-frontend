7381c9207114c4b6d07aa4742abbb938f83c50f3
allenorton <norton.allene@gmail.com>
2020-10-14 14:00:29 -0500

    fixed render for wine pairing

--- src/Components/PairResults.js
+++ src/Components/PairResults.js
@@ -7,36 +7,36 @@ class PairResults extends Component {
     //     winePairing: this.props.winePairing
     // }
 
-    pairObj = this.props.winePairing
+    // pairObj = this.props.winePairing
 
-    pairedWines = this.pairObj.pairedWines
-    desc = this.pairObj.pairingText
-    bottles = this.pairObj.productMatches
+    // pairedWines = this.props.winePairing.pairedWines
+    // desc = this.props.winePairing.pairingText
+    // bottles = this.props.winePairing.productMatches
     renderPairs = (pairs) => {
-        if (Object.keys(this.pairObj).includes('status')) { return undefined }
-        else if (Object.keys(this.pairObj).includes('pairedWines')) { return pairs.map(wine => <li>{wine}</li>) }
+        if (Object.keys(this.props.winePairing).includes('status')) { return undefined }
+        else if (Object.keys(this.props.winePairing).includes('pairedWines')) { return pairs.map(wine => <li>{wine}</li>) }
         else { return undefined }
     }
     renderWines = (wines) => {
-        if (Object.keys(this.pairObj).includes('status')) { return <p>{this.pairObj.message}</p> }
-        else if (Object.keys(this.pairObj).includes('pairedWines')) {
+        if (Object.keys(this.props.winePairing).includes('status')) { return <p>{this.props.winePairing.message}</p> }
+        else if (Object.keys(this.props.winePairing).includes('pairedWines')) {
             if (wines.length === 0) { return <p>Could not find a match. Please try again with another dish, ingredient, or cuisine type.</p> } else { return wines.map(wine => <RecResults key={wine.id} wine={wine} postWine={this.props.postWine} />) }}
         else { return undefined }
         }
 
         render() {
-            console.log(this.pairObj)
+            console.log(this.props.winePairing)
             return (
                 <div className="rec-results">
-                    {console.log(this.pairedWines)}
+                    {console.log(this.props.winePairing.pairedWines)}
                     <div className="paired-wines">
                         <ul>
-                            {this.renderPairs(this.pairedWines)}
+                            {this.renderPairs(this.props.winePairing.pairedWines)}
                         </ul>
                     </div>
-                    <p>{this.desc}</p>
+                    <p>{this.props.winePairing.pairingText}</p>
                     <div className="paired-bottle">
-                        {this.renderWines(this.bottles)}
+                        {this.renderWines(this.props.winePairing.productMatches)}
                     </div>
                 </div>
             );
