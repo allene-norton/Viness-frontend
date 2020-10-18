3264843def545bd57a962bc8f871d14a19d4dff0
allenorton <norton.allene@gmail.com>
2020-10-17 20:58:26 -0500

    added responsive right side navbar

--- src/Containers/NavBar.js
+++ src/Containers/NavBar.js
@@ -5,15 +5,33 @@ export default class NavBar extends Component {
     render() {
         return (
             <div className="viness-menu">
-                <nav>
-                    <ul className='menuItems'>
-                    <li name='home'><Link to="/home" >Home</Link></li>
-                    <li name='recommendations'><Link to="/recommendations" >Get Recommendation</Link></li>
-                    <li name='recommendations'><Link to="/wine_pairing" >Get Wine Pairing</Link></li>
-                    <li name='recommendations'><Link to="/food_pairing" >Get Food Pairing</Link></li>
-                    </ul>
+                <nav role="navigation">
+                    <div id="menuToggle">
+
+                        <input type="checkbox" />
+
+
+                        <span></span>
+                        <span></span>
+                        <span></span>
+
+
+                        <ul id="menu">
+                        <li name='home'><Link to="/home" ><button>Home</button></Link></li>
+                        <li name='recommendations'><Link to="/recommendations" ><button>Get Recommendation</button></Link></li>
+                        <li name='recommendations'><Link to="/wine_pairing" ><button>Get Wine Pairing</button></Link></li>
+                        <li name='recommendations'><Link to="/food_pairing" ><button>Get Food Pairing</button></Link></li>
+                        </ul>
+                    </div>
+                    {/* <ul className='menuItems'>
+                        <li name='home'><Link to="/home" >Home</Link></li>
+                        <li name='recommendations'><Link to="/recommendations" >Get Recommendation</Link></li>
+                        <li name='recommendations'><Link to="/wine_pairing" >Get Wine Pairing</Link></li>
+                        <li name='recommendations'><Link to="/food_pairing" >Get Food Pairing</Link></li>
+                    </ul> */}
                 </nav>
             </div>
         )
     }
 }
+
