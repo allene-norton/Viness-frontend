21ee5380e9d54b385d40887e2d4554b896ad6cb6
allenorton <norton.allene@gmail.com>
2020-10-18 18:15:27 -0500

    edited home page styling

--- src/Containers/Home.js
+++ src/Containers/Home.js
@@ -3,14 +3,18 @@ import React, { Component } from 'react'
 import SavedContainer from './SavedContainer'
 class Home extends Component {
 
- 
+
   render() {
     let user = this.props.currentUser
     return (
       <div className="home">
-        <h2>Viness Home Page</h2>
-        <h3>{user.display_name}</h3>
-        <img src={user.image} alt={user.display_name} />
+        {/* <h2>Viness Home Page</h2> */}
+        <div className='profile'>
+          <div className='profile-info'>
+            <h3>{user.display_name}</h3>
+            <img src={user.image} alt={user.display_name} />
+          </div>
+        </div>
         <div className='saved-wines'>
           <SavedContainer postComment={this.props.postComment} saved={this.props.saved} deleteWine={this.props.deleteWine} />
         </div>
