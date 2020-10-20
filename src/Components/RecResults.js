74954b0d466671433679ea819e73e6e409ab23f5
allenorton <norton.allene@gmail.com>
2020-10-19 20:28:11 -0500

    styled wine recs and pairing to render same as saved

--- src/Components/RecResults.js
+++ src/Components/RecResults.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom'
+import { Item } from 'semantic-ui-react'
 
 
 class RecResults extends Component {
 
     handleClick = () => {
         let userId = JSON.parse(sessionStorage.getItem('user')).id
-        this.props.postWine({...this.props.wine, user_id: userId})
+        this.props.postWine({ ...this.props.wine, user_id: userId })
         alert('Your wine has been saved to your profile!')
         // return <Link to='/home' />
     }
@@ -18,11 +19,46 @@ class RecResults extends Component {
         console.log(wine.title)
         return (
             <div className="rec-results">
-                <h1>{wine.title}</h1>
-                <img src={wine.imageUrl} alt={wine.title} />
-                <h3>{wine.description}</h3>
-                <h3>{wine.price}</h3>
-                <Link to='/home'><button onClick={this.handleClick} >Save Wine</button></Link>
+                <Item.Group>
+                    <Item className='saved-wine'>
+                        <Item.Image className='item-image' size='tiny' src={wine.imageUrl} />
+                        <Item.Content>
+                            <Item.Header>{wine.title}</Item.Header>
+                            <Item.Meta>Description</Item.Meta>
+                            <Item.Description>
+                                {wine.description}
+                            </Item.Description>
+                            <Item.Extra>
+                                <Link to='/home'><button onClick={this.handleClick} >Save Wine</button></Link>
+                            </Item.Extra>
+                        </Item.Content>
+                    </Item>
+                </Item.Group>
+                {/* <Grid.Row>
+                <Grid.Column> */}
+                {/* <Card>
+                    <Image src={wine.imageUrl} wrapped ui={false} id='card-image'/>
+                    <Card.Content>
+                        <Card.Header>{wine.title}</Card.Header>
+                        <Card.Meta>
+                            <span className='rating'>Rating: {wine.averageRating}</span>
+                            <span className='price'>Price: {wine.price}</span>
+                        </Card.Meta>
+                        <Card.Description>
+                            {wine.description}
+                        </Card.Description>
+                    </Card.Content>
+                    <Card.Content extra>
+                       <Link to='/home'><button onClick={this.handleClick} >Save Wine</button></Link>
+                    </Card.Content>
+                </Card> */}
+                {/* </Grid.Column>
+                </Grid.Row> */}
+                {/* // <h1>{wine.title}</h1>
+                // <img src={wine.imageUrl} alt={wine.title} />
+                // <h3>{wine.description}</h3>
+                // <h3>{wine.price}</h3>
+                // <Link to='/home'><button onClick={this.handleClick} >Save Wine</button></Link> */}
             </div>
         );
     }
--- src/Containers/WineRec.js
+++ src/Containers/WineRec.js
@@ -1,16 +1,19 @@
 import React, { Component } from 'react'
 import RecSearch from '../Components/RecSearch'
 import RecResults from '../Components/RecResults'
+import { Card } from 'semantic-ui-react'
+
 
 class WineRec extends Component {
-    
+
     renderWines = (wines) => {
         console.log(wines)
         if (Object.keys(wines).includes('status')) {
             return <p>{wines.message}</p>
-        } else if ((Object.keys(wines).includes('recommendedWines'))) { 
-            if (wines['recommendedWines'].length === 0) {return <p>Sorry, we don't currently have recommendations for that varietal. Please try another varietal.</p>}
-            else {return wines['recommendedWines'].map(wine => <RecResults key={wine.id} wine={wine} postWine={this.props.postWine} />)} }
+        } else if ((Object.keys(wines).includes('recommendedWines'))) {
+            if (wines['recommendedWines'].length === 0) { return <p>Sorry, we don't currently have recommendations for that varietal. Please try another varietal.</p> }
+            else { return wines['recommendedWines'].map(wine => <RecResults key={wine.id} wine={wine} postWine={this.props.postWine} />) }
+        }
         else { return undefined }
     }
     render() {
@@ -18,7 +21,14 @@ class WineRec extends Component {
             <div className="rec-page">
                 <h2>Wine Recommendations</h2>
                 <div><RecSearch getWineRec={this.props.getWineRec} renderResultsList={this.renderResultsList} /></div>
-                <div>{this.renderWines(this.props.wines)}</div>
+                {/* <Grid celled centered stackable columns={4}> */}
+                
+                    <div>
+                    <Card.Group itemsPerRow={3}>
+                        {this.renderWines(this.props.wines)}
+                        </Card.Group>
+                        </div>
+                {/* </Grid> */}
             </div>
         );
     }
