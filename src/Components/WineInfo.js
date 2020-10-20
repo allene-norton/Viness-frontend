1b4baa4aed32b585dbae87e44b1f8433f05e7748
allenorton <norton.allene@gmail.com>
2020-10-20 12:31:22 -0500

    rendered star icons for rating wine info

--- src/Components/WineInfo.js
+++ src/Components/WineInfo.js
@@ -1,26 +1,38 @@
 import React, { Component } from 'react';
+import { Icon } from 'semantic-ui-react'
 import CommentsContainer from '../Containers/CommentsContainer'
 
+const wine = JSON.parse(sessionStorage.getItem('wineInfo'))
 class WineInfo extends Component {
-
-    handleDelete = () => {
-        this.props.deleteWine(this.props.wine.id)
+    
+    renderRating = () => {
+        if (wine.averageRating < 0.4) {
+            return <div><Icon name='star' size='small' /></div>
+        } else if (wine.averageRating >= 0.4 && wine.averageRating < 0.6) {
+            return <div><Icon name='star' size='small' /><Icon name='star' size='small' /></div>
+        } else if (wine.averageRating >= 0.6 && wine.averageRating < 0.8) {
+            return <div><Icon name='star' size='small' /><Icon name='star' size='small' /><Icon name='star' size='small' /></div>
+        } else if (wine.averageRating >= 0.8 && wine.averageRating < 1) {
+            return <div><Icon name='star' size='small' /><Icon name='star' size='small' /><Icon name='star' size='small' /><Icon name='star' size='small' /></div>
+        } else if (wine.averageRating >= 1) {
+            return <div><Icon name='star' size='small' /><Icon name='star' size='small' /><Icon name='star' size='small' /><Icon name='star' size='small' /><Icon name='star' size='small' /></div>
+        } else{
+            return 'currently unavailable'
+        }
     }
 
     render() {
-        let wine = JSON.parse(sessionStorage.getItem('wineInfo'))
+        
         console.log(wine.title)
         return (
             <div className="wine-info">
-                <h1>I'm the Wine Info Page</h1>
                 <h1>{wine.title}</h1>
-                <img onClick={() => this.props.closeDisplay()} src={wine.imageUrl} alt={wine.title} />
+                <img src={wine.imageUrl} alt={wine.title} />
                 <h3>{wine.description}</h3>
                 <h3>{wine.price}</h3>
-                <h3>Rating: {wine.averageRating}</h3>
+                <h3>Rating: {this.renderRating()}</h3>
                 <h4>Comments:</h4>
                 <CommentsContainer wine={wine} postComment={this.props.postComment} />
-                <button>Delete Wine</button>
             </div>
         );
     }
