557862043b0f61b4098c36399aea64ccd78963cd
allenorton <norton.allene@gmail.com>
2020-10-21 19:56:09 -0500

    added error boundary

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import React from 'react';
 import Initial from './Containers/Initial'
 import 'semantic-ui-css/semantic.min.css'
 import './App.css';
+import ErrorBoundary from '../src/Containers/ErrorBoundary'
 
 
 
 function App() {
   return (
-      <div className="App">
-          <Initial />
-      </div>
+    <div className="App">
+      <ErrorBoundary>
+        <Initial />
+      </ErrorBoundary>
+    </div>
   );
 }
 
