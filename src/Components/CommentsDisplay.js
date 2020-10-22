a9ff3d5f4c0a0e8c879bdf45cab84d0f7824fc7e
allenorton <norton.allene@gmail.com>
2020-10-21 19:10:23 -0500

    message when there are no comments

--- src/Components/CommentsDisplay.js
+++ src/Components/CommentsDisplay.js
@@ -8,12 +8,14 @@ class CommentsDisplay extends Component {
         if (this.props.comments.length >= 1) {
             return this.props.comments.map(comment => <CommentJs key={comment.id} comment={comment} deleteComment={this.props.deleteComment} getUserWines={this.props.getUserWines}/>)
         } else if(this.props.comments === undefined) { return undefined }
+        else if (this.props.comments.length < 1) {return <p>Be the first to leave a comment!</p>}
         else {return undefined}
     }
 
     render() {
         return (
             <div className='comments-display'>
+                {console.log(this.props.comments)}
                 <Comment.Group size='large'>
                     <Header as='h3' dividing>
                         Comments
