938eebb65206f5f204f3e5f5159cbc4b19ac745c
allenorton <norton.allene@gmail.com>
2020-10-30 12:39:29 -0500

    added conditional logic for length of food pairings

--- src/Containers/FoodPair.js
+++ src/Containers/FoodPair.js
@@ -9,7 +9,11 @@ class FoodPair extends Component {
 
   renderFoods = (foods) => {
     if (Object.keys(this.props.foodPairing).includes('status')) { return <p style={{fontSize: '1.5rem'}}>{this.props.foodPairing.message}</p> }
-    else if (Object.keys(this.props.foodPairing).includes('pairings')) { return this.props.pairedRecipes.map(recipes => <FoodResults key={recipes[0].name} recipes={recipes}  />) }
+    else if (Object.keys(this.props.foodPairing).includes('pairings')) { 
+      if (this.props.pairedRecipes.length <= 0) {return <p style={{fontSize: '1.5rem'}}>Sorry, we couldn't find a match. Please try entering another varietal.</p>}
+      else {
+      return this.props.pairedRecipes.map(recipes => <FoodResults key={recipes[0].name} recipes={recipes}  
+      />) }}
     else { return undefined }
   }
   renderText = (text) => {
